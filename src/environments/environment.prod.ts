25d7eb858cc8102214559d0e9472fe4896c4ee1d
Roosevelt <panta_85.x@hotmail.com>
2018-01-05 13:53:10 -0500

    4- Creating a New Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBmYHWBXk5c1D3Ip51dbLQPAeolSf-FWOI",
+    authDomain: "oshop-574af.firebaseapp.com",
+    databaseURL: "https://oshop-574af.firebaseio.com",
+    projectId: "oshop-574af",
+    storageBucket: "",
+    messagingSenderId: "195716975423"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBmYHWBXk5c1D3Ip51dbLQPAeolSf-FWOI",
+    authDomain: "oshop-574af.firebaseapp.com",
+    databaseURL: "https://oshop-574af.firebaseio.com",
+    projectId: "oshop-574af",
+    storageBucket: "",
+    messagingSenderId: "195716975423"
+  }
 };
