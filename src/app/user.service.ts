7f38d8012ac52de32b2eadff0777595af132b0c5
Roosevelt <panta_85.x@hotmail.com>
2018-01-17 08:25:54 -0500

    11- Protecting the Admin Routes

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,6 @@
+import { AppUser } from './models/app-user';
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import * as firebase from 'firebase';
 
 @Injectable()
@@ -8,10 +9,13 @@ export class UserService {
   constructor(private db: AngularFireDatabase) { }
 
   save(user: firebase.User) {
-    this.db.object('/users' + user.uid).update({
+    this.db.object('/users/' + user.uid).update({
       name: user.displayName,
       email: user.email
     });
   }
 
+  get(uid: string): FirebaseObjectObservable<AppUser> {
+    return this.db.object('/users/' + uid);
+  }
 }
